6018a602ace667823506bbfe97345b799ca3211c
ub-victor <victoireushindi371@gmail.com>
2026-05-19 01:06:09 +0200

    member.schema fixed by adding dep zod

--- src/schemas/member.schema.ts
+++ src/schemas/member.schema.ts
@@ -6,7 +6,7 @@ export const createMemberSchema = z.object({
     email: z
         .string()
         .min(1, "Email is required")
-    .email("Email format is invalid")
+        .email("Email format is invalid")
     .trim(),
   githubUsername: z
     .string()
