94c9da97b4d1698cf3b22305dae2262ba1c8ccca
ub-victor <victoireushindi371@gmail.com>
2026-05-19 01:08:56 +0200

    parten.schema fixed by adding dep zod

--- src/schemas/partner.schema.ts
+++ src/schemas/partner.schema.ts
@@ -11,7 +11,7 @@ export const createPartnerSchema = z.object({
     email: z
         .string()
         .min(1, "Email is required")
-    .email("Email format is invalid")
+        .email("Email format is invalid")
     .trim(),
   partershipReason: z
     .string()
