993fbc78000903cdb6e2beaea3eaaff337f6c355
ub-victor <victoireushindi371@gmail.com>
2026-05-18 19:07:05 +0200

    Test the middleware and adjust the response

--- src/controllers/health.controller.ts
+++ src/controllers/health.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import response from "../utils/response";
 
 function checkHealth(_req: Request, res: Response) {
-  response.success(res, { status: "ok", uptime: process.uptime() }); 
+  response.success(res, { status: "ok", uptime: process.uptime() });
 }
 
 export default { checkHealth };
