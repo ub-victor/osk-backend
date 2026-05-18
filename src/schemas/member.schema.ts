2e78c12a8ddd50d6980edb19b1fb877eff21e9c6
ub-victor <victoireushindi371@gmail.com>
2026-05-19 01:05:59 +0200

    member.schema fixed by adding dep zod

--- src/schemas/member.schema.ts
+++ src/schemas/member.schema.ts
@@ -4,7 +4,7 @@ import { CodingLevel } from "../generated/prisma/client";
 export const createMemberSchema = z.object({
     name: z.string().min(1, "Name is required").trim(),
     email: z
-    .string()
+        .string()
     .min(1, "Email is required")
     .email("Email format is invalid")
     .trim(),
